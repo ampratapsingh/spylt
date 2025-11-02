e48ea1d80eeb37ff2bee50369fba96f9136a109d
ampratapsingh <amitsingh1007official@gmail.com>
2025-11-02 19:47:05 +0530

    first commit of this site

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import HeroSection from './sections/HeroSection'
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+      <HeroSection/>
+    </main>
+  )
+}
+
+export default App
